9cf25fed827de812eeae0925cdcf5a82581b829e
AhouraNiknam <36394665+AhouraNiknam@users.noreply.github.com>
2018-03-28 23:20:09 -0700

    CS485 Midterm - Asteroids

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float lifetime;
+
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
